c5dac0e76bd383a0ef7f9569e7798cea1997ab4e
Alouiscious Sims <info@reneesoul.com>
2019-06-10 03:53:02 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,13 +1,15 @@
 def unsafe?(speed)
 speed = 75
-if speed < 40 || speed > 60
-	"unsafe"
-else
-	'safe'
+	if speed < 40 || speed > 60
+		"unsafe"
+	else
+		'safe'
+	end
 end
 
 
 
 def not_safe?(speed)
-
+speed = 75
+if speed < 40 || speed > 60
 end
